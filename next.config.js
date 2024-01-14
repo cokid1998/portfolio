6af532f41b33d12b5c9230ad6599f2232333fe66
cokid1998 <cokid98@naver.com>
2024-01-14 18:05:40 +0900

    Fix image loader config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  images: {
+    loader: "imgix",
+    path: "https://cokid1998.github.io/portfolio/",
+  },
 };
 
 module.exports = nextConfig;
