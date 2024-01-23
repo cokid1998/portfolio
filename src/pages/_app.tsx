66da3ea66d14ec7ab010d665f30827407cad6e6b
cokid1998 <cokid98@naver.com>
2024-01-23 15:11:32 +0900

    Style: Font setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Jua, Noto_Sans_KR } from "next/font/google";
+
+const noto_sans_kr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={noto_sans_kr.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      circleBorderGreen: "#3a866d",
+      borderGreen: "#5CA991",
+      titleGreen: "18C990",
+      subtitleGreen: "#19CB91",
+      boxBgGreen: "#0a5840",
+      blackBg: "#101010",
+      grayBg: "#515151",
+      white: "FFFFFF",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
