87acddc3de29904906fa582a49ed4cff8cf6fbac
cokid1998 <cokid98@naver.com>
2024-01-23 22:37:42 +0900

    chore: Add tailwind-styled-components package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
+import RotationCircle from "@/src/Section1/RotationCircle";
+import tw from "tailwind-styled-components";
+
 export default function Home() {
   return (
-    <main
-      className={`flex flex-col justify-center items-center min-h-screen bg-grayBg font-semibold`}
-    >
-      <div className={`text-6xl`}>LEE TAE GWAN</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
+    <main>
+      <FirstSection>
+        <div
+          className={`w-96 h-96 border-circleBorderGreen border-4 rounded-full absolute`}
+        />
+        <div className={`text-6xl`}>LEE TAE GWAN</div>
+        <div className={`text-5xl`}>PORTFOLIO</div>
+        <div className={`text-lg`}>cokid98@gmail.com</div>
+      </FirstSection>
     </main>
   );
 }
+
+const FirstSection = tw.section`
+  flex
+  flex-col
+  gap-5
+  justify-center
+  items-center
+  min-h-screen
+  bg-grayBg
+  font-semibold
+  relative
+`;
